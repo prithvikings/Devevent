a8a921702ed139457cf40270b6554ab1643505e4
prithvikings <prithvi07raj07@gmail.com>
2025-10-26 22:03:34 +0530

    Add MongoDB integration with Mongoose and define Event and Booking models

---
+++ src/lib/mongoose.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let cached = global.mongoose;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  // Return existing connection promise if one is in progress
+  if (!cached.promise) {
+    // Validate MongoDB URI exists
+    if (!MONGODB_URI) {
+      throw new Error(
+        "Please define the MONGODB_URI environment variable inside .env.local"
+      );
+    }
+    const options = {
+      bufferCommands: false, // Disable Mongoose buffering
+    };
+
+    // Create a new connection promise
+    cached.promise = mongoose.connect(MONGODB_URI, options).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (err) {
+    cached.promise = null;
+    throw err;
+  }
+  return cached.conn;
+}
+
+export default connectDB;
